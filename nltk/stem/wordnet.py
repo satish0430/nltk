e3366732f2efe95096e93662542bee45033f7983
Sion1225 <visiopo44@naver.com>
2024-05-26 17:48:59 +0900

    lemmatize_text with (lemma, pos)

--- nltk/stem/wordnet.py
+++ nltk/stem/wordnet.py
@@ -1,6 +1,6 @@
 # Natural Language Toolkit: WordNet stemmer interface
 #
-# Copyright (C) 2001-2023 NLTK Project
+# Copyright (C) 2001-2024 NLTK Project
 # Author: Steven Bird <stevenbird1@gmail.com>
 #         Edward Loper <edloper@gmail.com>
 # URL: <https://www.nltk.org/>
@@ -9,8 +9,6 @@
 from typing import List
 
 from nltk.corpus import wordnet as wn
-from nltk.tag import pos_tag
-from nltk.tokenize import word_tokenize
 
 
 class WordNetLemmatizer:
@@ -19,19 +17,6 @@ class WordNetLemmatizer:
 
     Lemmatize using WordNet's built-in morphy function.
     Returns the input word unchanged if it cannot be found in WordNet.
-
-        >>> from nltk.stem import WordNetLemmatizer
-        >>> wnl = WordNetLemmatizer()
-        >>> print(wnl.lemmatize('dogs'))
-        dog
-        >>> print(wnl.lemmatize('churches'))
-        church
-        >>> print(wnl.lemmatize('aardwolves'))
-        aardwolf
-        >>> print(wnl.lemmatize('abaci'))
-        abacus
-        >>> print(wnl.lemmatize('hardrock'))
-        hardrock
     """
 
     def lemmatize(self, word: str, pos: str = "n") -> str:
@@ -45,69 +30,54 @@ class WordNetLemmatizer:
             for satellite adjectives.
         :type pos: str
         :return: The lemma of `word`, for the given `pos`.
+
+            >>> from nltk.stem import WordNetLemmatizer
+            >>> wnl = WordNetLemmatizer()
+            >>> print(wnl.lemmatize('dogs'))
+            dog
+            >>> print(wnl.lemmatize('churches'))
+            church
+            >>> print(wnl.lemmatize('aardwolves'))
+            aardwolf
+            >>> print(wnl.lemmatize('abaci'))
+            abacus
+            >>> print(wnl.lemmatize('hardrock'))
+            hardrock
         """
         lemmas = wn._morphy(word, pos)
         return min(lemmas, key=len) if lemmas else word
 
+    def lemmatize_text(self, text: str) -> List[str]:
+        """
+        Tokenize input text, estimate the part-of-speech tag of each word,
+        and return a list of lemmas and pos tag.
+
+        Returns each input word unchanged when it cannot be found in WordNet.
+
+        :param text: The input text to lemmatize.
+        :type text: str
+        :return: A list with the estimated lemma and pos tag of each `word` in the input text.
+
+            >>> from nltk.stem import WordNetLemmatizer
+            >>> wntl = WordNetLemmatizer().lemmatize_text
+            >>> print(wntl('Proverbs are short sentences drawn from long experience.'))
+            ['Proverbs', 'be', 'short', 'sentence', 'draw', 'from', 'long', 'experience', '.']
+            >>> print(wntl('proverbs are short sentences drawn from long experience.'))
+            ['proverb', 'be', 'short', 'sentence', 'draw', 'from', 'long', 'experience', '.']
+        """
+        from nltk.tag import pos_tag
+        from nltk.tokenize import word_tokenize
+
+        return [
+            # Lemmatize each POS-tagged word:
+            (self.lemmatize(word, self.tag2pos(tag)), tag)
+            # Tokenize the input text and POS tag each word:
+            for word, tag in pos_tag(word_tokenize(text))
+        ]
+
+    @staticmethod
+    def tag2pos(tag):
+        return {"N": "n", "V": "v", "J": "a", "R": "r"}.get(tag[0], "n")
+
     def __repr__(self):
         return "<WordNetLemmatizer>"
-
-
-class TextLemmatizer:
-    """
-    WordNet Text Lemmatizer
-
-    Lemmatize from corpus using word_tokenize, WordNet's built-in morphy function and pos_tag.
-    Returns the input word unchanged if it cannot be found in WordNet.
-
-        >>> from nltk.stem import TextLemmatizer
-        >>> text_wnl = TextLemmatizer()
-        >>> print(text_wnl.lemmatize('Proverbs are short sentences drawn from long experience.'))
-        ['Proverbs', 'be', 'short', 'sentence', 'draw', 'from', 'long', 'experience', '.']
-        >>> print(text_wnl.auto_lemmatize('proverbs are short sentences drawn from long experience.'))
-        ['proverb', 'be', 'short', 'sentence', 'draw', 'from', 'long', 'experience', '.']
-    """
-
-    # POS tag dict for matching with WordNet's lemmatize
-    pos_word_dict = {
-        "VBP": "v",
-        "VB": "v",
-        "VBG": "v",
-        "VBD": "v",
-        "VBN": "v",
-        "VBZ": "v",
-        "JJ": "a",
-        "JJR": "a",
-        "JJS": "a",
-        "RB": "r",
-        "RBR": "r",
-        "RBS": "r",
-        "NN": "n",
-        "NNS": "n",
-        "NNP": "n",
-        "NNPS": "n",
-    }
-
-    def lemmatize(self, sentence: str) -> List:
-        """Automatically lemmatize `word` with out pos using pos_tag and WordNet's built-in morphy function.
-        Returns the input word unchanged if it cannot be found in WordNet.
-
-        :param word: The input word to lemmatize.
-        :type word: str
-        :return: The list for lemma of `word`, for automatically estimates `pos`.
-        """
-        # Tokenize the sentence
-        words = word_tokenize(sentence)
-
-        # POS tagging
-        pos_tags = pos_tag(words)
-
-        # Lemmatize the words
-        lemma_list = []
-        for i, word in enumerate(words):
-            lemma = WordNetLemmatizer().lemmatize(
-                word, self.pos_word_dict.get(pos_tags[i][1], "n")
-            )  # word.lower() can be used but it is trade-off problem
-            lemma_list.append(lemma)
-
-        return lemma_list
